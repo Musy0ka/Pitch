1e22262e3597d6587a70a4d43e936b7e2f3f96b7
Musy0ka <allan.musyoka@student.moringaschool.com>
2022-05-20 16:35:27 +0300

    added initializer

---
+++ postpitch/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e6e90bb07913ed20136db70fd6c3ecf0'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from postpitch import routes
